da744d772e8daf9157aba7cd8eb7e63bc2360ffa
Irene <25118334+isteves@users.noreply.github.com>
2018-06-21 15:27:02 -0700

    fixed bug; should now be more robust to differences in eml structure (when some fields are filled out vs not, etc.)

--- R/es_access.R
+++ R/es_access.R
@@ -15,17 +15,36 @@
 #' }
 
 es_access <- function(eml, path = NULL) {
+    
     get_access_spice <- function(x){
-        dplyr::tibble(fileName = x$physical$objectName,
-               name = x$entityName,
-               contentUrl = x$physical$distribution$online$url$url,
-               fileFormat = x$physical$dataFormat$externallyDefinedFormat$formatName)
+        x %>% 
+            unlist() %>% 
+            tibble::enframe() %>% 
+            dplyr::mutate(name = dplyr::case_when(
+                grepl("objectName", name) ~ "fileName",
+                grepl("entityName", name) ~ "name",
+                grepl("url", name) ~ "contentUrl",
+                grepl("formatName", name) ~ "fileFormat"
+            )) %>% 
+            na.omit() %>% 
+            filter(value != "download") %>% #often also included as url
+            spread(name, value)
     }
+
+    entities <- c("dataTable", "spatialRaster", "spatialVector", "storedProcedure", "view", "otherEntity")
+    entities <- entities[entities %in% names(eml$dataset)]
     
-    access_dt <- lapply(eml$dataset$dataTable, get_access_spice)
-    access_oe <- lapply(eml$dataset$otherEntity, get_access_spice)
+    entity_objs <- purrr::map(entities, ~eml2::eml_get(eml, .x)) %>% 
+        # restructure so that all entities are at the same level
+        purrr::map_if(~!is.null(.x$entityName), list) %>% 
+        unlist(recursive = FALSE) 
     
-    out <- dplyr::bind_rows(access_dt, access_oe)
+    access_entities <- lapply(entity_objs, get_access_spice)
+    
+    out <- dplyr::bind_rows(access_entities)
+    
+    #reorder
+    out <- out[, c("fileName", "name", "contentUrl", "fileFormat")]
     
     return(out)
     
